bb87134c4f4c3e7d2eb7a8c03ce6fa2fec58f017
Sn Kinos <dony435@naver.com>
2020-04-03 00:16:49 +0900

    Initial commit

---
+++ src/practice.ts
@@ -0,0 +1,20 @@
+let count = 0;
+count += 1;
+// count = '갑분문'; // 절 대 안 되 지
+
+const message: string = 'hello world';
+console.log(message);
+
+const done: boolean = true;
+
+const numbers: number[] = [1,2,3];
+const messages: string[] = ['hello', 'world'];
+
+// messages.push(1) // 절 대 안 되 지
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+let color: 'red' | 'orange' | 'yellow' = 'red';
+color = 'yellow';
+// color = 'green'; // 절 대 안 되 지
