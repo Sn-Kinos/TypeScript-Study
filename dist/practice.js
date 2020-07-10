cc62e183e1340f3182bf89cc09da562c392e7554
Sn Kinos <dony435@naver.com>
2020-07-10 17:55:37 +0900

    ¯\_(ツ)_/¯

--- dist/practice.js
+++ dist/practice.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 function sum(x, y) {
     return x + y;
 }
@@ -15,6 +26,7 @@ function returnNothing() {
 var Circle = /** @class */ (function () {
     function Circle(radius) {
         this.radius = radius;
+        this.radius = radius;
     }
     Circle.prototype.getArea = function () {
         return this.radius * this.radius * Math.PI;
@@ -23,6 +35,8 @@ var Circle = /** @class */ (function () {
 }());
 var Rectangle = /** @class */ (function () {
     function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
         this.width = width;
         this.height = height;
     }
@@ -35,3 +49,23 @@ var shapes = [new Circle(5), new Rectangle(317, 68)];
 shapes.map(function (value) {
     console.log(value.getArea());
 });
+var circle = new Circle(5);
+var person = {
+    name: '미우',
+    age: 20,
+};
+var expert = {
+    name: '에리스',
+    skills: ['C#', 'Unity'],
+};
+var people = [person, expert];
+function merge(a, b) {
+    return __assign(__assign({}, a), b);
+}
+var merged = merge({ miu: 1 }, { sella: 1 });
+function wrap(param) {
+    return {
+        param: param
+    };
+}
+var wrapped = wrap(10);
